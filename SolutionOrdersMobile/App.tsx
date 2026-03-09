2541391ac3edd3fd2045971ed8d9bbbe862a0cbb
Michał Woźniak <wozniak.michal.2004@gmail.com>
2026-03-09 20:28:06 +0100

    Refactor App component to utilize ScrollView and integrate Greeting components

--- SolutionOrdersMobile/App.tsx
+++ SolutionOrdersMobile/App.tsx
@@ -6,11 +6,13 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text, ScrollView } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
+import Greeting from './src/components/Greeting';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -20,10 +22,16 @@ function App() {
     //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
     //   <AppContent />
     // </SafeAreaProvider>
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native!!</Text>
-      <Text style={styles.subtitle}>with TypeScript 🚀</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Hello React Native!!</Text>
+    //   <Text style={styles.subtitle}>with TypeScript 🚀</Text>
+    // </View>
+     <ScrollView style={styles.container}>
+      <Greeting name="Anna" age={25} />
+      <Greeting name="Piotr" isVip={true} />
+      <Greeting name="Kasia" age={30} isVip={true} />
+      <Greeting name="Jan" />
+    </ScrollView>
   );
 }
 
@@ -49,20 +57,27 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#f5f5f5',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
-    marginTop: 8,
-  },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#f5f5f5',
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     color: '#333',
+//   },
+//   subtitle: {
+//     fontSize: 16,
+//     color: '#666',
+//     marginTop: 8,
+//   },
+// });
 
 export default App;
